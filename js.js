1c675a6b4420e3e44a445a4fe1ef14259e6ac47b
Chandum2plus <chandukumarbca@gmail.com>
2024-02-15 12:06:31 +0530

    code of the string

--- js.js
+++ js.js
@@ -1,1 +1,23 @@
-console.log("Hello world !")
+console.log("Hello world !");
+const str="This is the first String";
+const str2='This is the second String';
+const str3=`This is the third String Primitive`;
+console.table([str,str2,str3]);
+
+const string4 = new String("A String object");
+console.log(string4);
+
+console.log("Chandu kumar".charAt(7));
+
+console.log("Chintu"[2])
+
+const a="a";
+const b="b";
+
+if(a>b){
+    console.log(`${a} is greater than ${b}`);
+}else if(a<b){
+    console.log(`${a} is less than ${b}`);
+}else{
+    console.log(`${a} and ${b} both are equal`);
+}
